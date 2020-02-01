550db4c45c3220ddc15bb3e11367ec4d17d4e601
Shaan <Shaan@admins-MacBook-Pro.local>
2020-02-01 09:05:06 -0800

    users can submit and save posts

--- server.js
+++ server.js
@@ -2,13 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-app.get('/', function (req, res) {
-    res.render('home');
-});
+require('./controllers/posts')(app);
+// Set db
+require('./data/reddit-db');
+
+
+app.get('/', (req, res) => res.render('home'))
+app.get('/posts/new', (req, res) => res.render('posts-new'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
