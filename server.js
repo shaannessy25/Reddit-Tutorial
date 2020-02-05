f3467d0d857836c27f83f4002747028b418566b9
Shaan <Shaan@admins-MacBook-Pro.local>
2020-02-04 16:02:59 -0800

    fixed new post route

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const exphbs  = require('express-handlebars');
-const bodyParser = require('body-parser');
-const expressValidator = require('express-validator');
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
 
 // Use Body Parser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 
 // Add after body parser initialization!
 app.use(expressValidator());
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
-//requirements
-require('./controllers/posts')(app);
-require('./data/reddit-db');
+// set db
+require("./data/reddit-db");
 
+// app.get('/', (req, res) => res.render('posts-index'))
+app.get("/posts/new", (req, res) => res.render("posts-new"));
 
-app.get('/', (req, res) => res.render('posts-index'))
-app.get('/posts/new', (req, res) => res.render('posts-new'))
+//Controllers
+require("./controllers/posts")(app);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-
-
-
-
-
-
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
