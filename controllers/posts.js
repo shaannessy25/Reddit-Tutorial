cd849d5716f5344189671a5e24c9be6121dc4ffe
Shaan <Shaan@admins-MacBook-Pro.local>
2020-02-04 21:15:08 -0800

    Users can see subreddits

--- controllers/posts.js
+++ controllers/posts.js
@@ -30,4 +30,14 @@ module.exports = app => {
             console.log(err.message);
           });
     });
+      // SUBREDDIT
+    app.get("/n/:subreddit", function(req, res) {
+      Post.find({ subreddit: req.params.subreddit })
+        .then(posts => {
+          res.render("posts-index", { posts });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
 };
