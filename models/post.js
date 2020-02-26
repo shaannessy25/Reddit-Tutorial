7180c91c19d406e85458d338b4e85f549e4fd524
Shaan <Shaan@admins-MacBook-Pro.local>
2020-02-26 15:39:44 -0800

    fixing users login and logout

--- models/post.js
+++ models/post.js
@@ -2,26 +2,27 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
+    author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     title: { type: String, required: true },
     url: { type: String, required: true },
     summary: { type: String, required: true },
     subreddit: { type: String, required: true },
     comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
 });
-  
-module.exports =  mongoose.model("Post", PostSchema)
+
+module.exports = mongoose.model("Post", PostSchema)
 
 
 // PostSchema.pre("save", function(next) {
 //     // SET createdAt AND updatedAt
 //     const now = new Date();
 //     this.updatedAt = now;
-  
+
 //     if (!this.createdAt) {
 //       this.createdAt = now;
 //     }
-  
+
 //     next();
 //   });
-  
+
 //   module.exports = mongoose.model("Post", PostSchema);
--- server.js
+++ server.js
@@ -1,44 +1,74 @@
-require('dotenv').config()
-let cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
-const express = require("express");
-const app = express();
-const port = 3000;
+// Require Libraries
+const express = require('express');
 
-const exphbs = require("express-handlebars");
-const bodyParser = require("body-parser");
-const expressValidator = require("express-validator");
+var cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
+// App Setup
+const app = express();
+
+app.use(cookieParser()); // Add this after you initialize express.
+
+require('dotenv').config();
+// Middleware
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+
 
 // Use Body Parser
 app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
-app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Handlebars
-app.engine("handlebars", exphbs());
-app.set("view engine", "handlebars");
+// Set db
+require('./data/reddit-db');
 
 
-app.use(expressValidator());
+var checkAuth = (req, res, next) => {
+    console.log("Checking authentication");
+    if (typeof req.cookies.nToken === "undefined" || req.cookies.nToken === null) {
+        req.user = null;
+    } else {
+        var token = req.cookies.nToken;
+        var decodedToken = jwt.decode(token, { complete: true }) || {};
+        req.user = decodedToken.payload;
+    }
 
+    next();
+};
+
+
+app.use(checkAuth);
+
+// Routes
+// app.get('/', (req, res) => {
+//     res.render('layouts/main.handlebars');
+//   });
+
+app.get("/posts/new", (req, res) => {
+    const currentUser = req.user._id;
+    res.render("posts-new", {
+        currentUser
+    })
+});
 
 
 //Controllers
 require("./controllers/posts")(app);
-require('./controllers/comments')(app);
+require('./controllers/comments.js')(app);
 require('./controllers/auth.js')(app);
 
-// set db
-require("./data/reddit-db");
+// Start Server
 
-// app.get('/', (req, res) => res.render('posts-index'))
-app.get("/posts/new", (req, res) => res.render("posts-new"));
+app.listen(3000, () => {
+    console.log('Reddit listening on port localhost:3000!');
+});
 
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 module.exports = app;
